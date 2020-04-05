a9781bc5b9fa735b1f86f4ea1c2d66b792fac389
MFella <czarusiek112@wp.pl>
2020-04-05 16:10:31 +0200

    Routing added

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,20 +12,22 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(public authServ: AuthService, private alertify: AlertifyService) { }
+  constructor(public authServ: AuthService, 
+    private alertify: AlertifyService, private router: Router) { }
 
   ngOnInit() {
 
   }
 
   login(){
-   
     this.authServ.login(this.model).subscribe(next => {
 
       this.alertify.success('Success of logging!');
     }, error => {
       this.alertify.error(error);
-    })
+    }, ()=> {
+      this.router.navigate(['/members']);
+    });
   }
 
   loggedIn(){
@@ -34,5 +37,6 @@ export class NavComponent implements OnInit {
   logout(){
     localStorage.removeItem('token');
     this.alertify.message('Logged out!');
+    this.router.navigate(['/home']);
   }
 }
